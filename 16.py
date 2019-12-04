126ca994f1e8a55d3cf50e1b3a38a13327b71178
JeissonNaveros16 <j.naveros@uniandes.edu.co>
2019-12-04 17:45:01 +0000

    Punto 16

--- 16.py
+++ 16.py
@@ -1,11 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-d = np.loadtxt("monthrg.dat")
-anio = d[:,0]
-mes = d[:,1]
-dia = d[:,2]
-manchas = d[:,3]
+def FT(x):
+	N = len(x)
+	X = np.zeros(N, dtype=complex)
+	for k in range(N):
+		X[k] = 0.0j
+		for n in range(N):
+			X[k] += x[n] * np.exp(-2.0 * np.pi * 1.0j / N ) ** (k * n)
+	return X
+
+
+da = np.loadtxt("monthrg.dat")
+anio = da[:,0]
+mes = da[:,1]
+dia = da[:,2]
+manchas = da[:,3]
 
 ii = np.where(anio >= 1900)
 
@@ -22,8 +32,22 @@ for i in range(len(a)):
     
 tiempo = np.array(tiempo)
 
-plt.figure(figsize=(15,8))
+M = FT(ma)
+
+plt.figure(figsize=(8,15))
+
+plt.subplot(2,1,1)
+plt.scatter(np.arange(len(M)), np.abs(M/len(M)))
+plt.stem(np.arange(len(M)), np.abs(M/len(M)), use_line_collection=True)
+plt.xlabel('k')
+plt.ylabel('|$M_k$|/N')
+plt.title("N=" + str(len(M)))
+plt.grid()
+
+plt.subplot(2,1,2)
 plt.plot(tiempo, ma)
-plt.ylabel("Manchas solares")
-plt.xlabel("Timepo(dias)")
+plt.title("Manchas solares en función del tiempo")
+plt.ylabel("Manchas")
+plt.xlabel("Tiempo(dias)")
+
 plt.savefig("solar.png")
